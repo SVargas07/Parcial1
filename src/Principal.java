4953345b6a937acd6fe7a6143cd7e8c2d966a718
SVargas07 <s.vargasl23@uniandes.edu.co>
2024-02-21 09:18:01 -0500

    Commit 3

---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+
+	public static void main(String[] args) 
+	{
+		TMB calcular = new TMB (65.4, 1.75, 18);
+		
+		calcular.calcularTMBHombre();
+		
+	}
+
+}
--- src/TMB.java
+++ src/TMB.java
@@ -1,11 +1,32 @@
 
 public class TMB {
 
-	public static void main(String[] args) {
-		System.out.println("Prueba en git");
-		System.out.println("Segunda prueba de commit en github");
-		System.out.println("final.");
+	
+	private double peso;
+	private double altura;
+	private int edad;
+	
 
+	
+	public TMB (double paramPeso, double paramAltura, int paramEdad)
+	
+	{
+		this.peso = paramPeso;
+		this.altura =paramAltura;
+		this.edad =paramEdad;
+		
 	}
-
+	
+	
+	public double calcularTMBHombre() 
+	{
+        return 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * edad);
+	}
+	
+	public double calcularTMBmujer() {
+        return 447.593 + (9.247 * peso) + (3.098 * altura) - (4.33 * edad);
+    }
 }
+
+
+
