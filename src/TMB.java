70eed3bb6335d82d37b282230f2560fa32cdd59e
SVargas07 <s.vargasl23@uniandes.edu.co>
2024-02-21 08:22:22 -0500

    commit 2

--- src/TMB.java
+++ src/TMB.java
@@ -3,6 +3,8 @@ public class TMB {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba en git");
+		System.out.println("Segunda prueba de commit en github");
+		System.out.println("final.");
 
 	}
 
